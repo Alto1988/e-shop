20d05d20bb6e1108d1b6462bc42d263f4b8c1b01
Jacob Pagan <jake2415@gmail.com>
2021-07-30 09:40:11 -0500

    Fixed the template issues

--- shop_backend/shop/urls.py
+++ shop_backend/shop/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import index, product_list
+from .views import product_detail, product_list
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<slug:category>/', product_list, name='product_list'),
-    path('<int:id>/<slug:slug>/', index, name='product_detail'),
+    path('', product_list, name='product_list'),
+    path('<slug:category_slug>/', product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', product_detail, name='product_detail'),
 ]
